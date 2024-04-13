bb8e63cc61b67f864768ab106923f4c30bdb1fc5
JunSuU98 <qoreh1216@gmail.com>
2024-04-13 15:59:56 +0900

    init: 로그인 관련 컨트롤러 파일과 JSP 파일들을 추가

---
+++ src/main/java/su/login/controller/IdCheckController.java
@@ -0,0 +1,17 @@
+package su.login.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import su.member.control.Controller;
+import su.member.handler.MemberHandlerAdapter;
+
+public class IdCheckController implements Controller {
+
+	@Override
+	public MemberHandlerAdapter execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
